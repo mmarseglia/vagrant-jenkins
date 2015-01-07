8228c15d0ce6c2b8dfab172b9a54fa1446d4052d
Mike Marseglia <mike.marseglia@hasbro.com>
2015-01-07 17:14:49 -0500

    changed vagrant vm names so they don't conflict with another project. added git to both VMs

--- provision/provision.sh
+++ provision/provision.sh
@@ -14,14 +14,15 @@ cat > /etc/hosts <<EOH
 192.168.138.14 node.vagrant.vm node 
 EOH
 
+# install git, java. required on both VMs
+sudo yum install git java-1.6.0-openjdk
+
 # Bootstrap the master
 if [ "$1" == 'master' ]; then
 	sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
 	sudo rpm --import http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key
-	sudo yum -y install jenkins java-1.6.0-openjdk
+	sudo yum -y install jenkins 
 	sudo chkconfig --add jenkins
 	sudo chkconfig jenkins on
 	sudo service jenkins start
-else
-	sudo yum -y install java-1.6.0-openjdk
 fi
